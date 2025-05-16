155d497b5aae43fb4ee4cb9dd84a884b7cfe2c0f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-16 16:16:49 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -199,12 +199,6 @@ function swiper_sliders() {
             },
 
         },
-
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-
         pagination: {
             el: '.swiper-pagination',
         },
