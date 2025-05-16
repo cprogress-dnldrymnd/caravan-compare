ca672d80500c428cf48cb122114080fbf56fb512
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-16 15:22:40 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -75,6 +75,73 @@ function fancybox() {
 }
 
 function swiper_sliders() {
+
+    if (jQuery('.swiper-thumbnails').length > 0) {
+
+        if (window.innerWidth > 991) {
+            $height = jQuery('.swiper-gallery').outerHeight();
+
+            jQuery('.swiper-thumbnails').css('height', $height + 'px');
+            var swiper_thumb = new Swiper(".swiper-thumbnails", {
+                direction: "vertical",
+                spaceBetween: 10,
+                slidesPerView: 'auto',
+                freeMode: true,
+                watchSlidesProgress: true,
+            });
+        } else {
+            var swiper_thumb = new Swiper(".swiper-thumbnails", {
+                spaceBetween: 5,
+                slidesPerView: 4,
+                freeMode: true,
+                watchSlidesProgress: true,
+            });
+        }
+
+
+
+        var swiper_gallery = new Swiper('.swiper-gallery', {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            thumbs: {
+                swiper: swiper_thumb,
+            },
+            navigation: {
+                nextEl: '.swiper-gallery-next',
+                prevEl: '.swiper-gallery-prev',
+            },
+            pagination: {
+                el: '.swiper-gallery-pagination',
+                type: "fraction",
+            },
+        });
+        $key++;
+    } else {
+        jQuery('.swiper-gallery').each(function (index, element) {
+            var $id = 'swiper' + $key;
+            jQuery(this).attr('id', $id);
+            jQuery(this).find('.swiper-button-next').attr('id', $id + '-next');
+            jQuery(this).find('.swiper-button-prev').attr('id', $id + '-prev');
+            jQuery(this).find('.swiper-pagination').attr('id', $id + '-pagination');
+
+            var $id = new Swiper('#' + $id, {
+                loop: true,
+                slidesPerView: 1,
+                spaceBetween: 0,
+                navigation: {
+                    nextEl: '#' + $id + '-next',
+                    prevEl: '#' + $id + '-prev',
+                },
+                pagination: {
+                    el: '#' + $id + '-pagination',
+                    type: "fraction",
+                },
+            });
+            $key++;
+        });
+    }
+
     var swiper = new Swiper(".swiper-hero", {
         slidesPerView: 1,
 
