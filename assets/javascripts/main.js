4bf02bb3baad4ae607f559203abd84dae4887c22
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-21 13:23:44 +0800

    css

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -11,13 +11,13 @@ jQuery(document).ready(function () {
 function password_input() {
     jQuery('.password---hidden').click(function (e) {
         $input = jQuery(this).prev();
-        $input.attr('type', 'password');
+        $input.attr('type', 'text');
         jQuery(this).addClass('password---show').removeClass('password---hidden');
         e.preventDefault();
     });
     jQuery('.password---show').click(function (e) {
         $input = jQuery(this).prev();
-        $input.attr('type', 'text');
+        $input.attr('type', 'password');
         jQuery(this).addClass('password---hidden').removeClass('password---show');
         e.preventDefault();
     });
